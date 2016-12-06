ff9e981317d994064962cb2635601c1d69eab347
Joaquim Filipe Braga Simões Romero <filipe.romero@sapo.pt>
2016-12-06 18:51:37 +0000

    database script

---
+++ db.sql
@@ -0,0 +1,83 @@
+-- --------------------------------------------------------
+-- Host:                         remote-mysql3.servage.net
+-- Server version:               5.5.53-MariaDB-1~wheezy - mariadb.org binary distribution
+-- Server OS:                    debian-linux-gnu
+-- HeidiSQL Version:             9.3.0.4984
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table filipe.keyboard
+CREATE TABLE IF NOT EXISTS `keyboard` (
+  `taskId` int(11) DEFAULT NULL,
+  `timeStamp` int(11) DEFAULT NULL,
+  `keyPressed` varchar(500) DEFAULT NULL
+) ENGINE=Aria DEFAULT CHARSET=latin1 PAGE_CHECKSUM=1;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table filipe.mouse_clicks
+CREATE TABLE IF NOT EXISTS `mouse_clicks` (
+  `taskId` int(11) DEFAULT NULL,
+  `timeStamp` int(11) DEFAULT NULL,
+  `x` int(11) DEFAULT NULL,
+  `y` int(11) DEFAULT NULL,
+  `vizElement` varchar(500) DEFAULT NULL
+) ENGINE=Aria DEFAULT CHARSET=latin1 PAGE_CHECKSUM=1;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table filipe.mouse_pos
+CREATE TABLE IF NOT EXISTS `mouse_pos` (
+  `taskId` int(11) DEFAULT NULL,
+  `timeStamp` int(11) DEFAULT NULL,
+  `x` int(11) DEFAULT NULL,
+  `y` int(11) DEFAULT NULL,
+  `height` int(11) DEFAULT NULL,
+  `width` int(11) DEFAULT NULL
+) ENGINE=Aria DEFAULT CHARSET=latin1 PAGE_CHECKSUM=1 COMMENT='mouse positions for each task';
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table filipe.scrolling
+CREATE TABLE IF NOT EXISTS `scrolling` (
+  `taskId` int(11) DEFAULT NULL,
+  `timeStamp` int(11) DEFAULT NULL,
+  `scrollTop` int(11) DEFAULT NULL
+) ENGINE=Aria DEFAULT CHARSET=latin1 PAGE_CHECKSUM=1;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table filipe.task
+CREATE TABLE IF NOT EXISTS `task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tasktype` int(11) DEFAULT NULL,
+  `interface` int(11) DEFAULT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `email` varchar(500) DEFAULT NULL,
+  `gender` varchar(500) DEFAULT NULL,
+  `age` varchar(500) DEFAULT NULL,
+  `nationality` varchar(500) DEFAULT NULL,
+  `expertise` varchar(500) DEFAULT NULL,
+  `education` varchar(500) DEFAULT NULL,
+  `mwl_mental` varchar(500) DEFAULT NULL,
+  `mwl_physical` varchar(500) DEFAULT NULL,
+  `mwl_temporal` varchar(500) DEFAULT NULL,
+  `mwl_performance` varchar(500) DEFAULT NULL,
+  `mwl_effort` varchar(500) DEFAULT NULL,
+  `mwl_frustration` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=Aria DEFAULT CHARSET=latin1 PAGE_CHECKSUM=1;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
